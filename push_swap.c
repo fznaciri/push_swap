d6fdec919509ebac95d0c13f156bc53586002dea
Fatima ezzahra Naciri ben saghir <fatimaezzahranaciri0@gmail.com>
2021-04-23 16:37:30 +0000

    stack_min

--- push_swap.c
+++ push_swap.c
@@ -1,19 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fnaciri- <fnaciri-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/18 01:30:35 by mac               #+#    #+#             */
-/*   Updated: 2021/04/23 14:57:28 by fnaciri-         ###   ########.fr       */
+/*   Created: 2021/04/23 14:55:59 by fnaciri-          #+#    #+#             */
+/*   Updated: 2021/04/23 15:13:38 by fnaciri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "include/push_swap.h"
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+// find min value
 
-#include "stack.h"
+int stack_min(stack *a, int *min)
+{
+    int i;
+    int pos;
+    
+    i = 0;
+	while (a)
+	{
+		if (a->value < a->next->value)
+		{
+			*min = a->value;
+			pos = i; 
+		}
+		a = a->next;
+		i++;
+	}
+    return (i);
+}
 
-#endif
+//get it to top of stack a (either ra or rra depends on size of a)
+
+//push it to stack b
